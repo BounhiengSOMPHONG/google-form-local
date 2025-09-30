13ef602a73e2bbdcacb60187a21cd54c9c459390
bounhieng <Teng.ios123@gmail.com>
2025-09-30 09:25:23 +0700

    login or register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,7 +42,7 @@
             <div class="flex items-center space-x-4">
                 @if (Route::has('register'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
-                        {{ __('Register') }}
+                        {{ __('Create account') }}
                     </a>
                 @endif
                 <x-primary-button class="ms-3">
