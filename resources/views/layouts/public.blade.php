acc22a79ee1382393ab0ad94e4005b42d3766515
bounhieng <Teng.ios123@gmail.com>
2025-10-10 15:09:29 +0700

    fix favicon

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -6,6 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Favicon -->
+        <link rel="icon" href="{{ asset('images/Tplus-logo-01.png') }}" type="image/png" />
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
